f723a73f89165c0fd185e66a2f3d7fec865e9578
Jorge Diaz <jorgeddc@gmail.com>
2021-09-18 22:18:13 +0200

    a por todas

---
+++ javaScript.js
@@ -0,0 +1,57 @@
+const hamburguer = document.querySelector('.hamburguer')
+const aboutMe = document.getElementById('about-me')
+const navegador = document.querySelector('.navegador')
+const circle1  = document.querySelector('.circles1')
+const circle2  = document.querySelector('.circles2')
+const circle3  = document.querySelector('.circles3')
+const circle4  = document.querySelector('.circles4')
+
+
+hamburguer.addEventListener('click', e=>{
+    e.preventDefault();
+    navegador.classList.toggle('active')
+    document.querySelector('.barra1').classList.toggle('active')
+ 
+    
+    document.querySelector('.navegador-grande').style.top='13px'
+})
+
+aboutMe.addEventListener('click', e=>{
+    e.preventDefault()
+    document.querySelector(' .perfil-img').classList.toggle('active')
+    document.querySelector('.container__main-home-aboutme').classList.toggle('active')
+})
+
+aboutMe.addEventListener('click', e =>{
+    e.preventDefault()
+    document.querySelector('.navegador-grande').style.top='-150px'
+})
+
+circle1.addEventListener('mouseover', ()=>{
+
+    document.querySelectorAll('span')[0].style.opacity='1'
+    document.querySelectorAll('span')[0].style.top='-20px'
+  
+
+})
+circle2.addEventListener('mouseover', ()=>{
+
+    document.querySelectorAll('span')[1].style.opacity='1'
+    document.querySelectorAll('span')[1].style.top='-20px'
+  
+
+})
+circle3.addEventListener('mouseover', ()=>{
+
+    document.querySelectorAll('span')[2].style.opacity='1'
+    document.querySelectorAll('span')[2].style.top='-20px'
+  
+
+})
+circle4.addEventListener('mouseover', ()=>{
+
+    document.querySelectorAll('span')[3].style.opacity='1'
+    document.querySelectorAll('span')[3].style.top='-20px'
+  
+
+})
